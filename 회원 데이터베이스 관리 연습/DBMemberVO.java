334a88b202bf19319bfbd28a3e35920deb279afd
GWOODEN_LAPTOP\wjdgk <gwooden22@gmail.com>
2022-12-13 22:18:04 +0900

    불편 값 클래스

---
+++ 회원 데이터베이스 관리 연습/DBMemberVO.java
@@ -0,0 +1,34 @@
+package DB2;
+
+public class MemberVO {
+	
+	private int memberno;
+	private String id;
+	private String name;
+
+
+	
+	public int getMemberno() {
+		return memberno;
+	}
+	public void setMemberno(int memberno) {
+		this.memberno = memberno;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+
+		
+	
+
+}
